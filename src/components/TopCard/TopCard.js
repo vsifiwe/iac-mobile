fd161ccda962212251ec62be805145f58377db4f
Manzi Asifiwe <asifiwemanzi@gmail.com>
2022-04-12 14:03:04 +0200

    add axios

--- src/components/TopCard/TopCard.js
+++ src/components/TopCard/TopCard.js
@@ -2,6 +2,7 @@ import { View, Text, ImageBackground, ActivityIndicator } from "react-native";
 import React, { useState, useEffect } from "react";
 import styles from "./TopCard.style";
 import { COLORS } from "../../styles";
+import axios from "axios";
 
 const image = require("../../../assets/background.png");
 
@@ -11,12 +12,14 @@ const TopCard = () => {
 
 	useEffect(() => {
 		setLoading(true);
-		fetch("https://iac-backend.herokuapp.com/events")
-			.then((res) => res.json())
-			.then((data) => {
-				setEvents(data.events);
-				setLoading(false);
-			});
+		axios.get('https://iac-backend.herokuapp.com/events', {
+			params: {
+				email: 'asifiwemanzi@gmail.com'
+			}
+		}).then(res => setEvents(res.data.events))
+		.then(() => setLoading(false))
+		.catch(err => console.error(err))
+		
 	}, []);
 
 	return (
@@ -25,10 +28,20 @@ const TopCard = () => {
 				{loading ? (
 					<ActivityIndicator size="large" color={COLORS.PRIMARY} />
 				) : (
+
+					events.length !== 0 ?
 					<>
 						<Text style={styles.title}>Your next appointment:</Text>
 						<Text style={styles.body}>{events[0].summary}</Text>
 						<Text style={styles.time}>18:30</Text>
+					</>
+
+					:
+
+					<>
+						<Text style={styles.title}>You do not have any appointment.</Text>
+						<Text style={styles.body}>Yaay! 🎉</Text>
+						{/* <Text style={styles.time}>18:30</Text> */}
 					</>
 				)}
 			</ImageBackground>
